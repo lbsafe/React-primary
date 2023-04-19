df2f2bbc61d6ae8a63c363bb5b1a67641fc8efd8
lbsafe <lbsafe@naver.com>
2023-04-19 13:48:13 +0900

    React 스터디 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(){
+  return <header>
+    <h1><a href="/">React_study</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">메뉴1</a></li>
+      <li><a href="/read/2">메뉴2</a></li>
+      <li><a href="/read/3">메뉴3</a></li>          
+    </ol>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>welcome React! 컴포넌트</h2>
+    hello, 오건희
+  </article>
+}
 function App() {
   return (
     <div className="App">
-      Hello React! -오건희-
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
