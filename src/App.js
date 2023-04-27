331b4c9a556cb20517278d4d93a63e31c003c820
lbsafe <lbsafe@naver.com>
2023-04-27 09:34:48 +0900

    React props 스터디

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(){
+function Header(props){
+  console.log('props', props, props.title)
   return <header>
-    <h1><a href="/">React_study</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
-function Nav(){
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">메뉴1</a></li>
-      <li><a href="/read/2">메뉴2</a></li>
-      <li><a href="/read/3">메뉴3</a></li>          
+        {lis}
     </ol>
   </nav>
 }
-function Article(){
+function Article(props){
   return <article>
-    <h2>welcome React! 컴포넌트</h2>
-    hello, 오건희
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome! React!" body="Hello, 건희"></Article>
     </div>
   );
 }
